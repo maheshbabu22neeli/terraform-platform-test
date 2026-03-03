aff7ca9761f34624b16876f1ecce23a950ad9cc0
Mahesh Babu Neeli <maheshbabu22.neeli@gmail.com>
2026-03-03 23:07:31 +1100

    Terraform Module User - Using Module code to create VPC, Internet Gateway, Subnets and Peer VPC Connection

--- aws-vpc/vpc.tf
+++ aws-vpc/vpc.tf
@@ -2,4 +2,5 @@ module "vpc" {
   source = "git::https://github.com/maheshbabu22neeli/terraform-platform.git//aws-vpc?ref=main"
   project = var.project
   environment = var.environment
+  is_peering_required = true
 }
